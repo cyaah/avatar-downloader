9de4216adc832b7a3852ad4ed30bcf0a87421e6e
Calvin Yah <calvin.yah@gmail.com>
2018-02-19 22:45:32 +0000

    Authorizing token

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,17 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'd911d8d25df25f9e832fc2e039acd49d46200150'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
